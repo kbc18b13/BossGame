2bc5cb10053b8575fe205f8d2ac7655a50b5d7a2
kbc18b13 <kbc18b13@stu.kawahara.ac.jp>
2020-05-29 15:51:20 +0900

    エフェクト途中

---
+++ Engine/Effekseer/Effect.h
@@ -0,0 +1,32 @@
+#pragma once
+extern EffekseerRenderer::Renderer* g_effectRenderr;
+extern Effekseer::Manager* g_effectManager;
+
+namespace MyEngine{
+
+class Effect{
+public:
+	~Effect(){
+
+	}
+
+	void Init( const wchar_t* filepath , float scale = 1){
+		m_effect = Effekseer::Effect::Create( g_effectManager, (const EFK_CHAR*)filepath, scale );
+	}
+
+	void Play(){
+		m_hundle = g_effectManager->Play( m_effect, pos );
+	}
+
+	void SetRot(){
+		g_effectManager->getma
+	}
+
+private:
+	Effekseer::Handle m_hundle;
+	Effekseer::Vector3D m_pos;
+	Effekseer::Effect* m_effect;
+};
+
+}
+
